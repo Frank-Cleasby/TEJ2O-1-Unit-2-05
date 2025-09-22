1af9a5440a6f356d9c43aecc6a935fd09015a06e
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-09-22 12:20:55 -0400

    something

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Sep 2025
+ * This program gets the temperature in K
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
