f36e72a2e57761f30c0c9d36bdeaf91f69abde5d
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-09-24 17:11:42 +0000

    added python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Frank
+Created on: Sep 2025
+This module declares the temperature in Kelvin
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temperature = temperature()
+        display.clear()
+        display.show("The Temperature is:" + (temperature + 273) + "K.")
+        display.show(Image.HAPPY)
